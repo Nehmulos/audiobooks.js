9e2fddf69505a0f9db08e330a6fe4ce5d947a747
Nehmulos <nehmu@lavabit.com>
2013-01-31 00:38:53 +0100

    reformatted the code to suck less, or let's say it sucks in obvious ways now;

--- pub/src/TrackScreen.js
+++ pub/src/TrackScreen.js
@@ -1,55 +1,19 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-function TrackScreen(author, book)
-{
+function TrackScreen(author, book) {
     this.author = author;
     this.book = book;
 }
 
-TrackScreen.prototype.createNodes = function()
-{
+TrackScreen.prototype.createNodes = function() {
     var _this = this;
     
     app.toolbar.setAuthor(this.author);
     app.toolbar.setBook(this.book);
     
-    var buildNodesFromJson = function(data) 
-    {
-        $.each(data.cds, function(key, cd)
-        {
-            var cdName = this.name;
-            var $cdDiv = $("<div>"+ cdName +"</div>");
-            var $cdList = $("<ol/>");
-            $("#main").append($cdDiv);
-            $cdDiv.append($cdList);
-                
-            $.each(cd.tracks, function()
-            {
-                var $trackLi = $("<li/>");
-                var $playTrackLink = $("<a class='PlayTrackLink' href='"+window.location.hash+"'>"+this+"</a>");
-                $playTrackLink.text(this); 
-                $playTrackLink.attr("data-cd", cdName);
-                $playTrackLink.attr("data-track", this);
-                $playTrackLink.attr("data-file", "books/"+ _this.author +"/"+ _this.book +"/"+ cdName + "/"+ this);
-                $trackLi.append($playTrackLink);
-                $cdList.append($trackLi);
-            });
-        });
-        $(".PlayTrackLink").click(function() {
-            app.player.play($(this).attr("data-file"));
-        });
-    };
-    
+    //TODO don't use this shitty way of cache checking, use a proper getter
     if(app.fileCache.author[this.author] && app.fileCache.author[this.author].book) {
         buildNodesFromJson(app.fileCache.author[this.author].book[this.book]);
-    }
-    else
-    {
-        $.getJSON("api/book.json?author=" + _this.author +"&book="+ _this.book, function(data) 
-        {
+    } else {
+        $.getJSON("api/book.json?author=" + _this.author +"&book="+ _this.book, function(data) {
             //data.cds = Utils.sortObject(data.cds);
             
             data.cds.sort(function(a,b) {
@@ -65,13 +29,40 @@ TrackScreen.prototype.createNodes = function()
             }
             app.fileCache.author[_this.author].book = new Object();
             app.fileCache.author[_this.author].book[_this.book] = data;
-            buildNodesFromJson(data);
+            _this.buildNodesFromJson(data);
         });
     }
 }
 
+TrackScreen.prototype.buildNodesFromJson = function(data) {
+    var _this = this;
+    $.each(data.cds, function(key, cd) {
+        var cdName = this.name;
+        var $cdDiv = $("<div>"+ cdName +"</div>");
+        var $cdList = $("<ol/>");
+        $("#main").append($cdDiv);
+        $cdDiv.append($cdList);
+            
+        $.each(cd.tracks, function() {
+            var $trackLi = $("<li/>");
+            var $playTrackLink = $("<a class='PlayTrackLink' href='"+window.location.hash+"'>"+this+"</a>");
+            $playTrackLink.text(this); 
+            $playTrackLink.attr("data-cd", cdName);
+            $playTrackLink.attr("data-track", this);
+            $playTrackLink.attr("data-file", "books/" + _this.author + "/" +
+                                _this.book + "/" + cdName + "/" + this);
+            $trackLi.append($playTrackLink);
+            $cdList.append($trackLi);
+        });
+    });
+    
+    $(".PlayTrackLink").click(function() {
+        var tracks = [];
+        tracks.push($(this).attr("data-file"));
+        app.player.playList(tracks);
+    });
+}
 
-TrackScreen.prototype.removeNodes = function()
-{
+TrackScreen.prototype.removeNodes = function() {
     $("#main").empty();
 }
