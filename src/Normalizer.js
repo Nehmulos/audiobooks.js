aeb31f2aabb3f4a74a49623da94d478e4a5d446c
Nehmulos <nehmu@lavabit.com>
2013-04-10 01:09:13 +0200

    horrific attempt to unify tracknames with numbers;

--- src/Normalizer.js
+++ src/Normalizer.js
@@ -2,10 +2,100 @@ var fs = require("fs"),
     path = require("path"),
     querystring = require("querystring"),
     gm = require("../node_modules/gm/index.js"),
-    exec = require('child_process').exec;
+    exec = require('child_process').exec,
+    fileServer = require("./FileServer.js");
 
 function Normalizer() {
 
+}
+
+Normaliser.prototype.unifyTrackNamesForBook = function(cdDirectory, finishCallback) {
+
+}
+
+Normaliser.prototype.unifyTrackNamesForCd = function(cdDirectory, finishCallback) {
+    fs.readdir(directory, function(error, files) {
+        var unhandledFiles = files.length;
+        var playableFiles = [];
+        var matches = [];
+        
+        // TODO horrible 
+        
+        // "1. CD 3 The 1st tale of xyz part 1"
+        // "10. CD 12 Another tale"
+        // unify to "01. CD 33 The 1st tale of xyz part 1"
+        function unify() {
+            var numberOfMatches = -1;
+            // [3] (matches) -> wut
+            var groups = {};
+            for (var i=1; i < matches.length; ++i) {
+                var m = matches[i];
+                var index = m.length;
+                if (!groups[index]) {
+                    groups[index] = [];
+                }
+                groups[index].push(m);
+            }
+            
+            for (key in groups) {
+                if (key != "length") {
+                    var lengths = new Array(groups[key]+1).join('0').split('');
+                    for (var i=0; i < groups[key].length; ++i) {
+                        if (lengths[i] < groups[key][i].length) {
+                            lengths[i] = groups[key][i].length;
+                        }
+                    }
+                    for (var i=0; i < lengths; ++i) {
+                        if (groups[key][i].length < lengths[i]) {
+                            playableFiles[i].replace(/([0-9]+)/, function(original, j) {
+                                if (j == i)
+                            })
+                        }
+                    }
+                }
+            }
+        }
+        
+        function checkEnd() {
+            --unhandledFiles;
+            if (unhandledFiles <= 0) {
+                handleImages();                  
+            }
+        }
+        
+        for(var i=0; i < files.length; ++i) {
+            var filename = files[i];
+            
+            if (!fileServer.isPlayableFile(filename)) {
+                checkEnd();
+                continue;
+            }
+            
+            /([0-9]+)/.exec(filename);
+        
+            fs.stat(path.join(directory, files[i]), (function(filename, i) {
+                return function(error, stats) {
+                    if(error) {
+                        console.log("can not read file " + filename);
+                        callback(error, []);
+                        return;
+                    }
+
+                    // handle other directories                        
+                    if(stats.isDirectory()) {
+                        handleDirectory(path.join(directory, filename));
+                    }
+                    
+                    if (stats.isFile() && fileServer.isImage(filename)) {
+                        potentialCovers.push(filename);
+                    }
+                    checkEnd();
+                }
+            })(filename, i);
+        }
+        
+        matches[i] = /([0-9]+)/.exec(trackName);
+    }
 }
 
 //TODO split into more separate functions
