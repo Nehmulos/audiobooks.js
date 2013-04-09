f9fc2359610f64fdafab58049ce9fbadfafe56ce
Nehmulos <nehmu@lavabit.com>
2013-04-10 01:43:05 +0200

    broken subversion,

--- src/Normalizer.js
+++ src/Normalizer.js
@@ -16,18 +16,30 @@ Normaliser.prototype.unifyTrackNamesForBook = function(cdDirectory, finishCallba
 Normaliser.prototype.unifyTrackNamesForCd = function(cdDirectory, finishCallback) {
     fs.readdir(directory, function(error, files) {
         var unhandledFiles = files.length;
-        var playableFiles = [];
-        var matches = [];
+        var tracks = {}
         
-        // TODO horrible 
+        for(var i=0; i < files.length; ++i) {
+            var filename = files[i];
+            
+            if (!fileServer.isPlayableFile(filename)) {
+                continue;
+            }
+            
+            tracks.push({
+                name: filename,
+                matches: /([0-9]+)/.exec(filename)
+            });
+        }
         
         // "1. CD 3 The 1st tale of xyz part 1"
         // "10. CD 12 Another tale"
         // unify to "01. CD 33 The 1st tale of xyz part 1"
-        function unify() {
+        for (var t=0; t < tracks.length; ++t) {
+            var matches = tracks[t].matches;
             var numberOfMatches = -1;
-            // [3] (matches) -> wut
             var groups = {};
+            
+            // group tracks by count of numberGroups /([0-9]+)/
             for (var i=1; i < matches.length; ++i) {
                 var m = matches[i];
                 var index = m.length;
@@ -38,63 +50,34 @@ Normaliser.prototype.unifyTrackNamesForCd = function(cdDirectory, finishCallback
             }
             
             for (key in groups) {
-                if (key != "length") {
-                    var lengths = new Array(groups[key]+1).join('0').split('');
-                    for (var i=0; i < groups[key].length; ++i) {
-                        if (lengths[i] < groups[key][i].length) {
-                            lengths[i] = groups[key][i].length;
-                        }
+                if (key == "length") {
+                    continue;
+                }
+                
+                var occurances = groups[key]
+                
+                // find the longest number for each match
+                // this obscure line creates an array filled with 0
+                var lengths = new Array(occurances.length+1).join('0').split('');
+                for (var i=0; i < occurances.length; ++i) {
+                    if (lengths[i] < occurances[i].length) {
+                        lengths[i] = occurances[i].length;
                     }
-                    for (var i=0; i < lengths; ++i) {
-                        if (groups[key][i].length < lengths[i]) {
-                            playableFiles[i].replace(/([0-9]+)/, function(original, j) {
-                                if (j == i)
-                            })
-                        }
+                }
+                
+                // put 0 infront of matches that are shorter than the longest number
+                for (var i=0; i < lengths.length; ++i) {
+                    if (occurances[i].length < lengths[i]) {
+                        tracks[t].replace(/([0-9]+)/, function(original, j) {
+                            if (j == i) {
+                                return  new Array(lengths[i]+1).join('0');
+                            }
+                            return original;
+                        });
                     }
                 }
             }
         }
-        
-        function checkEnd() {
-            --unhandledFiles;
-            if (unhandledFiles <= 0) {
-                handleImages();                  
-            }
-        }
-        
-        for(var i=0; i < files.length; ++i) {
-            var filename = files[i];
-            
-            if (!fileServer.isPlayableFile(filename)) {
-                checkEnd();
-                continue;
-            }
-            
-            /([0-9]+)/.exec(filename);
-        
-            fs.stat(path.join(directory, files[i]), (function(filename, i) {
-                return function(error, stats) {
-                    if(error) {
-                        console.log("can not read file " + filename);
-                        callback(error, []);
-                        return;
-                    }
-
-                    // handle other directories                        
-                    if(stats.isDirectory()) {
-                        handleDirectory(path.join(directory, filename));
-                    }
-                    
-                    if (stats.isFile() && fileServer.isImage(filename)) {
-                        potentialCovers.push(filename);
-                    }
-                    checkEnd();
-                }
-            })(filename, i);
-        }
-        
-        matches[i] = /([0-9]+)/.exec(trackName);
     }
 }
 
