ab465712931052a1b6d81d1fe0ce3236aa72c1ab
Nehmulos <nehmu@lavabit.com>
2014-01-27 21:07:52 +0100

    find the name of the first device in amixer and use it (instead of a hardcoded name);

--- src/Volume.js
+++ src/Volume.js
@@ -4,6 +4,7 @@ var fs = require("fs"),
     exec = require('child_process').exec;
 
 function Volume() {
+    this.findDeviceName();
 }
 
 Volume.prototype.set = function(res, volume) {
@@ -12,17 +13,12 @@ Volume.prototype.set = function(res, volume) {
         res.end('{"error": "invalid volume format. Must be a number (0-100)"}');
         return;
     }
-
-    exec("amixer set PCM " + volume + "%", function (error, stdout, stderr) {
+    
+    var self = this;
+    var command = "amixer set " + this.device + " " + volume + "%";
+    exec(command, function (error, stdout, stderr) {
         if (error) {
-            console.log("stdout " + stdout);
-            console.log("stderr " + stderr);
-            console.log("could not set volume: " + error);
-            
-            var errorObject = {error: ""+error};
-            
-            res.writeHead(500, {"Content-Type": "application/json"});
-            res.end(JSON.stringify(errorObject));
+            self.execError(stdout, stderr, command, error, res, self);
             return;
         }
         res.writeHead(200, {"Content-Type": "application/json"});
@@ -32,20 +28,15 @@ Volume.prototype.set = function(res, volume) {
 }
 
 Volume.prototype.get = function(res) {
-    exec("amixer get PCM", function (error, stdout, stderr) {
+    var self = this;
+    var command = "amixer get " + this.device;
+    exec(command, function (error, stdout, stderr) {
         if (error) {
-            console.log("stdout " + stdout);
-            console.log("stderr " + stderr);
-            console.log("could not get volume: " + error);
-            
-            var errorObject = {error: ""+error};
-            
-            res.writeHead(500, {"Content-Type": "application/json"});
-            res.end(JSON.stringify(errorObject));
+            self.execError(stdout, stderr, command, error, res, self);
             return;
         }
         var volumeMatches = /([0-9]+)%/.exec("" + stdout);
-        if (volumeMatches.length > 1) {
+        if (volumeMatches && volumeMatches.length > 1) {
             // send volume of first speaker (in most cases the left one)
             // IMPROVEMENT send a value for each speaker + speakerName
             res.writeHead(200, {"Content-Type": "application/json"});
@@ -55,6 +46,41 @@ Volume.prototype.get = function(res) {
         res.writeHead(500, {"Content-Type": "application/json"});
         res.end('{"status": "could not parse volume","error":"parseError"}');
     });
+}
+
+Volume.prototype.findDeviceName = function() {
+    if (this.device == "getting") {
+        return;
+    }
+    this.device = "getting";
+    var self = this;
+    exec("amixer", function (error, stdout, stderr) {
+        if (error) {
+            self.execError(stdout, stderr, command, error, res);
+            return;
+        }
+        var deviceMatch = /'(.+)'/.exec("" + stdout);
+        if (deviceMatch.length > 1) {
+            self.device = deviceMatch[1];
+        } else {
+            self.device = null;
+        }
+    });
+}
+
+Volume.prototype.execError = function(stdout, stderr, command, error, res, self) {
+    console.log("stdout " + stdout);
+    console.log("stderr " + stderr);
+    console.log(command);
+    console.log("could not set volume: " + error);
+    
+    var errorObject = {error: ""+error};
+    
+    res.writeHead(500, {"Content-Type": "application/json"});
+    res.end(JSON.stringify(errorObject));
+    if (self) {
+        self.findDeviceName();
+    }
 }
 
 
